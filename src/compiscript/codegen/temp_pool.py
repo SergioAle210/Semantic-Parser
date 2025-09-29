53fafdfb00477c98c0a6234eb3818cf38865f70a
SergioAle210 <sergioalejandro210@gmail.com>
2025-09-29 10:14:31 -0600

    Add temp_pool.py

---
+++ src/compiscript/codegen/temp_pool.py
@@ -0,0 +1,13 @@
+# compiscript/codegen/temp_pool.py
+from __future__ import annotations
+
+
+class TempPool:
+    def __init__(self, prefix: str = "t"):
+        self.prefix = prefix
+        self.n = 0
+
+    def new(self) -> str:
+        name = f"{self.prefix}{self.n}"
+        self.n += 1
+        return name
